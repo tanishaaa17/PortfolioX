4784337427d76eccd44e6f6d47344ceca03bb43d
tanishaaa17 <139698819+tanishaaa17@users.noreply.github.com>
2025-07-10 14:23:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
+import Navbar from '../components/Navbar';
 
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <main className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">👋 Hi, I'm Tanisha</h1>
+        <p className="mt-4 text-lg text-gray-600">A passionate web developer & designer.</p>
+      </main>
+    </div>
+  );
+}
